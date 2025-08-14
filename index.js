9693499615e9c6922e5157cac7f8bba8d15c9d55
Gabriel Donizetti <donizettigabriel3@gmail.com>
2025-08-14 13:09:28 -0300

    Desafio 2: Calculadora de Partidas Rankeadas
    :wq
    Conflicts:

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-let nome = "Spider";
-let xp = 7000;
-let nivel = "";
-
-if (xp < 1000) {
+function calculadorRank(vitorias, derrotas) {
+ const saldo = vitorias - derrotas;
+  
+ let nivel= "";  
+ if (saldo <= 10) {
     nivel = "Ferro";
-}
-else if (xp <= 2000) {
-    nivel = "Bronze";
-}
-else if (xp <= 5000) {
-    nivel = "Prata";
-}
-else if (xp <= 7000) {
-    nivel = "Ouro";
-}
-else if (xp <= 8000) {
-    nivel = "Platina";
-}
-else if  (xp <= 9000) {
-    nivel = "Ascendente";
-}
-else if (xp <= 10000) {
-    nivel = "Imortal"
-}
-else {
-    nivel = "Radiante"; 
-}
+     } else if (saldo <= 20) { 
+        nivel = "Bronze";
+      } else if (saldo <= 50) { 
+        nivel = "Prata";
+      } else if (saldo <= 80) {
+        nivel = "Ouro";
+      } else if (saldo <= 90) {
+        nivel = "Diamante";
+      } else if (saldo <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
 
-console.log(`O Heroi ${nome} tem ${xp} de XP e está no nível ${nivel}.`); 
+    return (saldo, nivel);
+    }
+
+    const vitorias = 55;
+    const derrotas = 10;
+
+    const {saldo, nivel} = calculadorRank(vitorias, derrotas);
+    console.log(`O Herói tem de saldo de ${saldo} e está no nível de ${nivel}.`);
+
